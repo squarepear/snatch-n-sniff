747a469e85cdcbacc7cc459ff1d6957eb3740f37
Miller <jadi.miller@bsu.edu>
2024-01-27 17:01:30 -0500

    Create main menu

---
+++ project/main_menu/main_menu.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+
+
+
+func _on_convention_button_pressed():
+	get_tree().change_scene_to_file("res://convention/convention.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+
+func _on_credits_pressed():
+	%CreditsLabel.show()
+
+
+func _on_ok_button_pressed():
+	%CreditsLabel.hide()
