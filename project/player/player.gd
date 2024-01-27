7712ac7293352da9c932cb885dbad24881bc0fc4
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-01-26 19:15:15 -0500

    Make Player able to turn

--- project/player/player.gd
+++ project/player/player.gd
@@ -8,13 +8,16 @@ const JUMP_VELOCITY = 4.5
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
+func _ready():
+	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -29,3 +32,11 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x))
+	if event is InputEventKey and event.is_action_pressed("ui_cancel"):
+		get_tree().quit() #im gamer
+		
