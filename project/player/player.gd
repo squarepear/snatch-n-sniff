5aa4c084165b6c8d9c8a8d099b4631b76b4f8503
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-01-26 21:46:09 -0500

    Add snatch logic

--- project/player/player.gd
+++ project/player/player.gd
@@ -7,6 +7,8 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@onready var snatch_detector := $SnatchDetector
+
 
 func _ready():
 	Input.mouse_mode=Input.MOUSE_MODE_CAPTURED
@@ -19,9 +21,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Handle movement.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -34,9 +34,20 @@ func _physics_process(delta):
 	move_and_slide()
 
 
+func snatch():
+	# COLIDE GET SB
+	snatch_detector.force_shapecast_update()
+	if !snatch_detector.is_colliding():
+		return
+	var sb :Node= snatch_detector.get_collider(0)
+	if sb.get_parent() is Sniffable:
+		sb.get_parent().snatch()
+
+
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
-		rotate_y(deg_to_rad(-event.relative.x))
-	if event is InputEventKey and event.is_action_pressed("ui_cancel"):
+		rotate_y(-event.relative.x*0.005)
+	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit() #im gamer
-		
+	if event.is_action_pressed("snatch"):
+		snatch()
