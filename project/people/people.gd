0f91162f12ff5bd59d4123af70713144bdc941da
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-01-28 13:09:32 -0500

    Improve people navigation

--- project/people/people.gd
+++ project/people/people.gd
@@ -10,11 +10,15 @@ extends CharacterBody3D
 @export var snatch_attempt_audio: AudioStream
 
 const SPEED = 1.0
+const POI_STOP_DISTANCE := 2.0
+const POI_STOP_TIME := 4.0
+const POI_STOP_TIME_RANGE := 2.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var chase_target: Node3D
 var current_poi: Node3D
+var waiting := false
 
 @onready var snatchback_detector = %SnatchbackDetector
 @onready var snatched_alarm: SnatchedAlarm = get_parent().get_node("SnatchedAlarm")
@@ -32,6 +36,10 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
+
+	if waiting and not chase_target:
+		return
+
 	look_at(chase_target.global_position if chase_target else current_poi.global_position)
 	rotation.x = 0
 	rotation.z = 0
@@ -46,10 +54,18 @@ func _physics_process(delta):
 	if chase_target:
 		_detect_target()
 
+	if global_position.distance_to(current_poi.global_position) <= POI_STOP_DISTANCE:
+		waiting = true
+		get_tree().create_timer(_calculate_stop_time()).timeout.connect(_pick_new_poi)
+
 
 func _pick_new_poi() -> void:
+	waiting = false
 	current_poi = points_of_interest[randi() % points_of_interest.size()]
 
+
+func _calculate_stop_time() -> float:
+	return POI_STOP_TIME + (randf() * POI_STOP_TIME_RANGE) - (POI_STOP_TIME_RANGE / 2.0)
 
 func get_snatch_attempt_audio() -> AudioStream:
 	return snatch_attempt_audio
